8b85e9dd5d290ee2740b9a4eb49464830286d22a
Mike Langin <michael.langin@platform.sh>
2022-01-10 14:58:24 -0600

    Directory push

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,9 +1,8 @@
 print("Welcome to the tip calculator.")
 total = float(input("What was the total bill? $"))
 people = int(input("How many people to split the bill? "))
-tip = (float(input("What tip percentage? "))/100)
-conv= 1.0 + tip
-final = (total * conv)/people
+tip = ((float(input("What tip percentage? "))/100)+1)
+final = (total * tip)/people
 print(f"Each person should pay: $" + "{:.2f}".format(final))
 
 
