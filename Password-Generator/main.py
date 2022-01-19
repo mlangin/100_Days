517ec0275122223f8068d79824d0d4622eb538f5
Mike Langin <michael.langin@platform.sh>
2022-01-19 16:27:19 -0600

    adding comment, change folder

--- Password-Generator/main.py
+++ Password-Generator/main.py
@@ -10,6 +10,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Randomizing the characters here, then again below is intentional.
 random.shuffle(letters)
 random.shuffle(numbers)
 random.shuffle(symbols)
